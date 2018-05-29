c647a5dae7b3ec18dfe87797116c5c6b5f9937ce
dethghost <admin@deth.org.ua>
2018-05-29 21:27:51 +0300

    init new repo

---
+++ AirportPanel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Tools.Db;
+
+namespace AirportPanel
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            var db = DB.GetInstanceDb().Connection();
+            db.Clone();
+            using (db)
+            {
+                db.Open();
+                var command = db.CreateCommand();
+                command.CommandText = "Create table plane(" +
+                                      "id int unsigned NOT NULL AUTO_INCREMENT," +
+                                      "Primary key (id))";
+                command.ExecuteNonQuery();
+                Console.WriteLine(db.Database);
+                Console.WriteLine(db.State);
+            }
+            Console.WriteLine(db.State);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Tools/Db/DB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace Tools.Db
+{
+    public class DB
+    {
+        private static DB instance;
+        private MySqlConnection connection;
+        private const string databaseName = "Data Source='localhost';" +
+                                            "Database='airport';" +
+                                            "UID='root';" +
+                                            "PWD='root';";
+        private DB()
+        {
+           connection = new MySqlConnection();
+           connection.ConnectionString = databaseName;
+        }
+
+        public static DB GetInstanceDb()
+        {
+            if (instance == null)
+            {
+                instance = new DB();
+                
+            }
+           
+            return instance;
+        }
+
+        public MySqlConnection Connection()
+        {
+            return connection;
+        }
+
+        private Object Clone()
+        {
+            return false;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
+        {
+            optionbuilder.UseSqlite(@"Data Source=d:\Sample.db");
+        }
+    }
+}
